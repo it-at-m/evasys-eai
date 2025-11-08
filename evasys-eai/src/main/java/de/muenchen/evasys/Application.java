92f15f0a0db5b102338caa43503e67a6bc4b036d
Fabinatix97 <fabian@fabi-online.de>
2025-11-08 08:49:03 +0100

    Add CommandLineRunner for quick testing

---
+++ evasys-eai/src/main/java/de/muenchen/evasys/Application.java
@@ -0,0 +1,32 @@
+package de.muenchen.evasys;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+
+import de.muenchen.evasys.service.SubunitService;
+import wsdl.soapserver_v100.SoapfaultMessage;
+import wsdl.soapserver_v100.UnitList;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+@SuppressWarnings("PMD.UseUtilityClass")
+public class Application {
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner testSoapClient(SubunitService subunitService) {
+        return args -> {
+            try {
+                UnitList list = subunitService.getSubunits();
+                System.out.println("Got " + list.getUnits().size() + " units!");
+            } catch (SoapfaultMessage e) {
+                e.printStackTrace();
+            }
+        };
+    }
+}

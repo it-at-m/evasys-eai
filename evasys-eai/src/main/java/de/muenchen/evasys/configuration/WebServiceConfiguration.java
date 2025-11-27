f56cd4dc50a6fc5b0d13f991641c08100e98bd82
Fabinatix97 <fabian@fabi-online.de>
2025-11-27 07:58:13 +0100

    Adjust web service configuration

---
+++ evasys-eai/src/main/java/de/muenchen/evasys/configuration/WebServiceConfiguration.java
@@ -0,0 +1,40 @@
+package de.muenchen.evasys.configuration;
+
+import de.lhm.pi.evasys.afs.SITrainingASIB;
+import de.muenchen.evasys.endpoint.SapServiceEndpoint;
+import de.muenchen.evasys.service.TrainingProcessorService;
+import jakarta.xml.ws.Endpoint;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebServiceConfiguration {
+    private final SapPoProperties props;
+
+    private final TrainingProcessorService trainingProcessorService;
+
+    public WebServiceConfiguration(final SapPoProperties props) {
+        this.props = props;
+        this.trainingProcessorService = null;
+    }
+
+    @Bean
+    public SITrainingASIB sapService() {
+        return new SapServiceEndpoint(trainingProcessorService);
+    }
+
+    @Bean
+    public Endpoint endpoint(final Bus bus, final SITrainingASIB sapService) {
+        final EndpointImpl endpoint = new EndpointImpl(bus, sapService);
+        final Map<String, Object> propsMap = new HashMap<>();
+        propsMap.put("ws-security.username", props.username());
+        propsMap.put("ws-security.password", props.password());
+        endpoint.setProperties(propsMap);
+        endpoint.publish("/evasyseai");
+        return endpoint;
+    }
+}

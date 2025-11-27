015dbf028b82cb1a62373799b5ad75f76fff0595
Fabinatix97 <fabian@fabi-online.de>
2025-11-27 08:04:05 +0100

    Create EvaSysService to encapsulate EvaSysClient logic

---
+++ evasys-eai/src/main/java/de/muenchen/evasys/service/EvaSysService.java
@@ -0,0 +1,39 @@
+package de.muenchen.evasys.service;
+
+import com.sap.document.sap.rfc.functions.ZLSOSTEVASYSRFC;
+import de.muenchen.evasys.client.EvaSysClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EvaSysService {
+
+    private final EvaSysClient client;
+
+    public EvaSysService(final EvaSysClient client) {
+        this.client = client;
+    }
+
+    public boolean trainerExists(final int trainerId, final int subunitId) {
+        return client.isTrainerExisting(trainerId, subunitId);
+    }
+
+    public void updateTrainer(final ZLSOSTEVASYSRFC trainingData) {
+        client.updateTrainer(trainingData);
+    }
+
+    public void insertTrainer(final ZLSOSTEVASYSRFC trainingData) {
+        client.insertTrainer(trainingData);
+    }
+
+    public boolean courseExists(final int courseId) {
+        return client.isCourseExisting(courseId);
+    }
+
+    public void updateCourse(final ZLSOSTEVASYSRFC trainingData) {
+        client.updateCourse(trainingData);
+    }
+
+    public void insertCourse(final ZLSOSTEVASYSRFC trainingData) {
+        client.insertCourse(trainingData);
+    }
+}

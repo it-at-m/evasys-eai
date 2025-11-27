c43b40c22b3003128566680ce635dd21b5e66fe6
Fabinatix97 <fabian@fabi-online.de>
2025-11-27 07:48:10 +0100

    Remove CommandLineRunner

--- evasys-eai/src/main/java/de/muenchen/evasys/Application.java
+++ evasys-eai/src/main/java/de/muenchen/evasys/Application.java
@@ -1,35 +1,14 @@
 package de.muenchen.evasys;
 
-import de.muenchen.evasys.service.SubunitService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Bean;
-import wsdl.soapserver_v100.SoapfaultMessage;
-import wsdl.soapserver_v100.UnitList;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@SuppressWarnings("PMD.UseUtilityClass")
 public class Application {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner testSoapClient(final SubunitService subunitService) {
-        return args -> {
-            try {
-                final UnitList list = subunitService.getSubunits();
-                LOGGER.info("Got {} units!", list.getUnits().size());
-            } catch (final SoapfaultMessage e) {
-                LOGGER.error("Error while fetching subunits", e);
-            }
-        };
     }
 }

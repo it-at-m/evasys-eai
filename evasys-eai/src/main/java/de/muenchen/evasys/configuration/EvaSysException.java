447bc0f547f8a4a555c27f87fcaa802759c5dd3e
Fabinatix97 <fabian@fabi-online.de>
2025-12-05 17:59:32 +0100

    Add another EvaSysException constructor for testing purposes

--- evasys-eai/src/main/java/de/muenchen/evasys/configuration/EvaSysException.java
+++ evasys-eai/src/main/java/de/muenchen/evasys/configuration/EvaSysException.java
@@ -3,6 +3,10 @@ package de.muenchen.evasys.configuration;
 @SuppressWarnings("serial")
 public class EvaSysException extends RuntimeException {
 
+    public EvaSysException(final String message) {
+        super(message);
+    }
+
     public EvaSysException(final String message, final Throwable cause) {
         super(message, cause);
     }

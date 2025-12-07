a0b3632bc7509bfe25ee891fc8795f73e3d07464
Fabinatix97 <fabian@fabi-online.de>
2025-12-07 07:22:56 +0100

    Make connection and receive timeouts configurable

--- evasys-eai/src/main/java/de/muenchen/evasys/configuration/EvaSysClientConfiguration.java
+++ evasys-eai/src/main/java/de/muenchen/evasys/configuration/EvaSysClientConfiguration.java
@@ -19,8 +19,8 @@ public class EvaSysClientConfiguration {
 
         final BindingProvider bp = (BindingProvider) port;
         bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, props.uri());
-        bp.getRequestContext().put("jakarta.xml.ws.client.connectionTimeout", 10_000);
-        bp.getRequestContext().put("jakarta.xml.ws.client.receiveTimeout", 30_000);
+        bp.getRequestContext().put("jakarta.xml.ws.client.connectionTimeout", props.connectionTimeout().toMillis());
+        bp.getRequestContext().put("jakarta.xml.ws.client.receiveTimeout", props.receiveTimeout().toMillis());
 
         @SuppressWarnings("rawtypes")
         final List<Handler> handlers = bp.getBinding().getHandlerChain();
